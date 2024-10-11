cbd0a7cc4ecb0e58038fd0880ac5b48d4b91a3d2
Jin Ah Kim <wlsdk3165@naver.com>
2024-10-12 00:11:51 +0900

    Update 소수.py

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,3 +1,4 @@
+#### 내 풀이 ####
 m,n=int(input()),int(input())
 q=set()
 for x in range(m,n+1):
@@ -9,3 +10,24 @@ if len(clear)==0:
     print(-1)
 else:
     print(sum(clear),min(clear))
+
+#### 다른 사람 풀이 ####
+M = int(input())
+N = int(input())
+a = []
+
+for i in range(M, N+1):
+    count = 0
+    for j in range(i+1):
+        if(i % (j+1) == 0):
+            count += 1
+    if(count == 2):
+        a.append(i)
+if(len(a) == 0):
+    print(-1)   
+else:
+    print(sum(a))
+    print(a[0])
+
+# 차이점 : 소수찾기 1978문제와 같다. 
+# 배울점 : X
