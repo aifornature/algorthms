96e36b80347afd0eb0ff620069ea242e17241755
Jin Ah Kim <wlsdk3165@naver.com>
2024-12-21 23:08:05 +0900

    Update 삼각형과 세 변.py

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -1,4 +1,5 @@
-while True:
+    #### 내 풀이 ####
+    while True:
     ans=sorted(list(map(int,input().split())),reverse=False)
     answer=''
     if ans[0]!=0 and (ans[0]+ans[1])<=ans[2]:
@@ -15,3 +16,19 @@ while True:
                 break
             answer='Scalene'
     print(answer)
+    #### 다른 사람 풀이 ####
+    while True :
+      a, b, c = map(int, input().split())
+      if a == b == c == 0 :
+        break
+      if sum((a, b, c)) - max((a, b, c)) <= max((a, b, c)) :
+        print("Invalid")
+      elif a == b == c :
+        print('Equilateral')
+      elif a == b or b == c or a == c :
+        print("Isosceles")
+      else :
+        print("Scalene")
+    #### 회고 ####
+    # 차이점 : -
+    # 배울점 : -
